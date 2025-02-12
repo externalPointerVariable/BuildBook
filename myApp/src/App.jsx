ef393e6bce2a98d997428ebbed2c670206eca65d
externalPointerVariable <vicky986847@gmail.com>
2025-02-12 15:10:54 +0000

    Added tailwind css to the project✅

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
      <div className="App">
-      <h1>My React App</h1>
+      <h1 class="text-3xl font-bold underline text-amber-300"> My react App </h1>
      </div>
     </>
   )
