5fbe54da768abc6bd3e63bc4c3c3bfe0b250b6e4
externalPointerVariable <vicky986847@gmail.com>
2025-02-14 15:08:12 +0000

    Edited index.css and added a component for designer horizontal line

---
+++ myApp/components/Horizontal.jsx
@@ -0,0 +1,8 @@
+function Horizontal() {
+    return (
+      <hr className="border-t-2 border-gray-300 my-6 mx-auto w-3/4 rounded-full shadow-md" />
+    );
+  }
+  
+  export default Horizontal;
+  
