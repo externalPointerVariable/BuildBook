e7c72df78aaa75e1d4440bf3430f5c16f983c7e9
externalPointerVariable <vicky986847@gmail.com>
2025-02-16 09:52:42 +0000

    Fixed the toggle menu

--- myApp/components/Toggle.jsx
+++ myApp/components/Toggle.jsx
@@ -11,12 +11,12 @@ export default function Toggle() {
   const [selectedProject, setSelectedProject] = useState(projects[0]);
 
   return (
-    <div className="flex flex-col w-full bg-inherit p-6">
-      <div className="flex">
+    <div className="flex flex-col w-full bg-inherit p-6 justify-around">
+      <div className="flex -mb-4.5">
         {projects.map((project) => (
           <button 
             key={project.name}
-            className={`px-6 py-2 font-bold rounded-t-2xl ${selectedProject.name === project.name ? "bg-white border-b-4" : "text-orange-400 bg-gray-600"}`} 
+            className={`px-6 py-2 font-bold rounded-t-2xl ${selectedProject.name === project.name ? "bg-white" : "text-orange-400 bg-gray-600"}`} 
             onClick={() => setSelectedProject(project)}>
             {project.name}
           </button>
