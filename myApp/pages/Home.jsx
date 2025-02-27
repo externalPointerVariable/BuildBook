1720de10ba3039500114f5b38eed4c7b7c726dda
externalPointerVariable <vicky986847@gmail.com>
2025-02-27 15:41:29 +0000

    Fix image import in Home component for correct asset reference

--- myApp/pages/Home.jsx
+++ myApp/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import BuildBookPNG from '../src/assets/BuildBook.png';
 
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen" id="home">
-        <img src="../src/assets/BuildBook.png" alt="BuildBook Logo" className="inline-block w-36 h-36 mr-2" />
+        <img src={BuildBookPNG} alt="BuildBook Logo" className="inline-block w-36 h-36 mr-2" />
       <h1 className="text-5xl font-bold text-gray-800 mb-4">
         Welcome to <span className="text-orange-400">BuildBook</span>
       </h1>
